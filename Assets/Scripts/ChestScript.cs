2a566e1f9181857c47a7ef19835e7443b8a34641
Heeme Beeme <heemebeeme@gmail.com>
2025-10-27 12:40:55 -0300

    Random and Rarities coded

--- Assets/Scripts/ChestScript.cs
+++ Assets/Scripts/ChestScript.cs
@@ -3,7 +3,15 @@ using UnityEngine;
 
 public class ChestScript : MonoBehaviour
 {
-    static System.Random ChestType = new System.Random();
+    static System.Random ChestTypeRnD = new System.Random();
+    static System.Random ChestLootRnD = new System.Random();
+
+    static int ChestTypeInt;
+    static string ChestTypeString;
+
+    static int ChestLootRarityInt;
+    static string ChestLootRarityString;
+
 
     #region Chest Types
     [Header("Chest Type")]
@@ -126,7 +134,142 @@ public class ChestScript : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.Space))
         {
-            
+            #region Get Chest Type
+
+            ChestTypeInt = ChestTypeRnD.Next(1, 1002);
+            ChestLootRarityInt = ChestLootRnD.Next(1, 1002);
+
+
+            if (ChestTypeInt < 501)
+            {
+                ChestTypeString = "Wood";
+
+                if (ChestLootRarityInt < 501)
+                {
+                    ChestLootRarityString = "Common";
+                }
+                else if (ChestLootRarityInt > 499 && ChestLootRarityInt < 801)
+                {
+                    ChestLootRarityString = "Uncommon";
+                }
+                else if (ChestLootRarityInt > 799 && ChestLootRarityInt < 951)
+                {
+                    ChestLootRarityString = "Rare";
+                }
+                else if (ChestLootRarityInt > 949 && ChestLootRarityInt < 1001)
+                {
+                    ChestLootRarityString = "Epic";
+                }
+                else if (ChestLootRarityInt == 1001)
+                {
+                    ChestLootRarityString = "Legendary";
+                }
+            }
+            else if (ChestTypeInt > 499 && ChestTypeInt < 801)
+            {
+                ChestTypeString = "Bronze";
+
+                if (ChestLootRarityInt < 301)
+                {
+                    ChestLootRarityString = "Common";
+                }
+                else if (ChestLootRarityInt > 299 && ChestLootRarityInt < 701)
+                {
+                    ChestLootRarityString = "Uncommon";
+                }
+                else if (ChestLootRarityInt > 699 && ChestLootRarityInt < 901)
+                {
+                    ChestLootRarityString = "Rare";
+                }
+                else if (ChestLootRarityInt > 899 && ChestLootRarityInt < 1001)
+                {
+                    ChestLootRarityString = "Epic";
+                }
+                else if (ChestLootRarityInt == 1001)
+                {
+                    ChestLootRarityString = "Legendary";
+                }
+            }
+            else if (ChestTypeInt > 799 && ChestTypeInt < 951)
+            {
+                ChestTypeString = "Silver";
+
+                if (ChestLootRarityInt < 201)
+                {
+                    ChestLootRarityString = "Common";
+                }
+                else if (ChestLootRarityInt > 199 && ChestLootRarityInt < 551)
+                {
+                    ChestLootRarityString = "Uncommon";
+                }
+                else if (ChestLootRarityInt > 549 && ChestLootRarityInt < 851)
+                {
+                    ChestLootRarityString = "Rare";
+                }
+                else if (ChestLootRarityInt > 849 && ChestLootRarityInt < 1001)
+                {
+                    ChestLootRarityString = "Epic";
+                }
+                else if (ChestLootRarityInt == 1001)
+                {
+                    ChestLootRarityString = "Legendary";
+                }
+            }
+            else if (ChestTypeInt > 949 && ChestTypeInt < 1001)
+            {
+                ChestTypeString = "Gold";
+
+                if (ChestLootRarityInt < 101)
+                {
+                    ChestLootRarityString = "Common";
+                }
+                else if (ChestLootRarityInt > 99 && ChestLootRarityInt < 348)
+                {
+                    ChestLootRarityString = "Uncommon";
+                }
+                else if (ChestLootRarityInt > 346 && ChestLootRarityInt < 748)
+                {
+                    ChestLootRarityString = "Rare";
+                }
+                else if (ChestLootRarityInt > 746 && ChestLootRarityInt < 998)
+                {
+                    ChestLootRarityString = "Epic";
+                }
+                else if (ChestLootRarityInt > 997)
+                {
+                    ChestLootRarityString = "Legendary";
+                }
+            }
+            else if (ChestTypeInt == 1001)
+            {
+                ChestTypeString = "Platinum";
+
+                if (ChestLootRarityInt < 51)
+                {
+                    ChestLootRarityString = "Common";
+                }
+                else if (ChestLootRarityInt > 49 && ChestLootRarityInt < 242)
+                {
+                    ChestLootRarityString = "Uncommon";
+                }
+                else if (ChestLootRarityInt > 240 && ChestLootRarityInt < 692)
+                {
+                    ChestLootRarityString = "Rare";
+                }
+                else if (ChestLootRarityInt > 690 && ChestLootRarityInt < 991)
+                {
+                    ChestLootRarityString = "Epic";
+                }
+                else if (ChestLootRarityInt > 990)
+                {
+                    ChestLootRarityString = "Legendary";
+                }
+            }
+
+            Debug.Log($"Chest Type Int: {ChestTypeInt}     Chest Loot Rarity Int: {ChestLootRarityInt}");
+            Debug.Log($"Chest Type = {ChestTypeString}     Chest Loot Rarity = {ChestLootRarityString}");
+
+            #endregion
         }
     }
 }
