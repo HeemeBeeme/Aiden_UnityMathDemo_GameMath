c317a28839123fecd0412f1388e52769ca2cf14d
Heeme Beeme <heemebeeme@gmail.com>
2025-10-29 20:04:19 -0300

    rewrote script from scratch
    no hard coded values, easily able to change in unity properties, all 5 chests and rarities

--- Assets/Scripts/ChestScript.cs
+++ Assets/Scripts/ChestScript.cs
@@ -5,271 +5,230 @@ public class ChestScript : MonoBehaviour
 {
     static System.Random ChestTypeRnD = new System.Random();
     static System.Random ChestLootRnD = new System.Random();
-
-    static int ChestTypeInt;
-    static string ChestTypeString;
-
-    static int ChestLootRarityInt;
-    static string ChestLootRarityString;
-
+    string currentChest;
+    string currentRarity;
+    public int ChestTypeInt;
+    public int ChestLootInt;
 
     #region Chest Types
     [Header("Chest Type")]
-    [SerializeField]
-    int WoodChest = 500;
-
-    [SerializeField]
-    int BronzeChest = 300;
-
-    [SerializeField]
-    int SilverChest = 150;
-
-    [SerializeField]
-    int GoldChest = 50;
-
-    [SerializeField]
-    int PlatinumChest = 1;
+    string WoodChest = "Wood Chest";
+    public int WoodChestChance = 500;
+    string BronzeChest = "Bronze Chest";
+    public int BronzeChestChance = 300;
+    string SilverChest = "Silver Chest";
+    public int SilverChestChance = 150;
+    string GoldChest = "Gold Chest";
+    public int GoldChestChance = 50;
+    string PlatinumChest = "Platinum Chest";
+    public int PlatinumChestChance = 1;
     #endregion
+
+    #region Chest Drops
 
     #region Wood Chest
     [Header("Wood Chest")]
-    [SerializeField]
-    int WoodCommon = 500;
-
-    [SerializeField]
-    int WoodUncommon = 300;
-
-    [SerializeField]
-    int WoodRare = 150;
-
-    [SerializeField]
-    int WoodEpic = 50;
-
-    [SerializeField]
-    int WoodLegendary = 1;
-    //1001
+    public int WoodCommon = 500;
+    public int WoodUncommon = 300;
+    public int WoodRare = 150;
+    public int WoodEpic = 50;
+    public int WoodLegendary = 1;
     #endregion
 
     #region Bronze Chest
     [Header("Bronze Chest")]
-    [SerializeField]
-    int BronzeCommon = 300;
-
-    [SerializeField]
-    int BronzeUncommon = 400;
-
-    [SerializeField]
-    int BronzeRare = 200;
-
-    [SerializeField]
-    int BronzeEpic = 100;
-
-    [SerializeField]
-    int BronzeLegendary = 1;
-    //1001
+    public int BronzeCommon = 300;
+    public int BronzeUncommon = 400;
+    public int BronzeRare = 200;
+    public int BronzeEpic = 100;
+    public int BronzeLegendary = 1;
     #endregion
 
     #region Silver Chest
     [Header("Silver Chest")]
-    [SerializeField]
-    int SilverCommon = 200;
-
-    [SerializeField]
-    int SilverUncommon = 350;
-
-    [SerializeField]
-    int SilverRare = 300;
-
-    [SerializeField]
-    int SilverEpic = 150;
-
-    [SerializeField]
-    int SilverLegendary = 1;
-    //1001
+    public int SilverCommon = 200;
+    public int SilverUncommon = 350;
+    public int SilverRare = 300;
+    public int SilverEpic = 150;
+    public int SilverLegendary = 1;
     #endregion
 
     #region Gold Chest
     [Header("Gold Chest")]
-    [SerializeField]
-    int GoldCommon = 100;
-
-    [SerializeField]
-    int GoldUncommon = 247;
-
-    [SerializeField]
-    int GoldRare = 400;
-
-    [SerializeField]
-    int GoldEpic = 250;
-
-    [SerializeField]
-    int GoldLegendary = 4;
-    //1001
+    public int GoldCommon = 100;
+    public int GoldUncommon = 247;
+    public int GoldRare = 400;
+    public int GoldEpic = 250;
+    public int GoldLegendary = 4;
     #endregion
 
     #region Platinum Chest
     [Header("Platinum Chest")]
-    [SerializeField]
-    int PlatinumCommon = 50;
-
-    [SerializeField]
-    int PlatinumUncommon = 191;
-
-    [SerializeField]
-    int PlatinumRare = 450;
-
-    [SerializeField]
-    int PlatinumEpic = 300;
-
-    [SerializeField]
-    int PlatinumLegendary = 10;
+    public int PlatinumCommon = 50;
+    public int PlatinumUncommon = 191;
+    public int PlatinumRare = 450;
+    public int PlatinumEpic = 300;
+    public int PlatinumLegendary = 10;
     #endregion
-
-    void Start()
-    {
-        
-    }
-
+    #endregion
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Space))
         {
-            #region Get Chest Type
+           ChestTypeInt = ChestTypeRnD.Next(1, 1002);
+            ChestLootInt = ChestLootRnD.Next(1, 1002);
 
-            ChestTypeInt = ChestTypeRnD.Next(1, 1002);
-            ChestLootRarityInt = ChestLootRnD.Next(1, 1002);
-
-
-            if (ChestTypeInt < 501)
+            if(ChestTypeInt <= WoodChestChance)
             {
-                ChestTypeString = "Wood";
+                currentChest = WoodChest;
 
-                if (ChestLootRarityInt < 501)
+                if(ChestLootInt <= WoodCommon)
                 {
-                    ChestLootRarityString = "Common";
+                    currentRarity = "Wood Common";
                 }
-                else if (ChestLootRarityInt > 499 && ChestLootRarityInt < 801)
-                {
-                    ChestLootRarityString = "Uncommon";
-                }
-                else if (ChestLootRarityInt > 799 && ChestLootRarityInt < 951)
-                {
-                    ChestLootRarityString = "Rare";
-                }
-                else if (ChestLootRarityInt > 949 && ChestLootRarityInt < 1001)
-                {
-                    ChestLootRarityString = "Epic";
-                }
-                else if (ChestLootRarityInt == 1001)
-                {
-                    ChestLootRarityString = "Legendary";
-                }
-            }
-            else if (ChestTypeInt > 499 && ChestTypeInt < 801)
-            {
-                ChestTypeString = "Bronze";
 
-                if (ChestLootRarityInt < 301)
+                else if (ChestLootInt > WoodCommon && ChestLootInt <= WoodCommon + WoodUncommon)
                 {
-                    ChestLootRarityString = "Common";
+                    currentRarity = "Wood Uncommon";
                 }
-                else if (ChestLootRarityInt > 299 && ChestLootRarityInt < 701)
-                {
-                    ChestLootRarityString = "Uncommon";
-                }
-                else if (ChestLootRarityInt > 699 && ChestLootRarityInt < 901)
-                {
-                    ChestLootRarityString = "Rare";
-                }
-                else if (ChestLootRarityInt > 899 && ChestLootRarityInt < 1001)
-                {
-                    ChestLootRarityString = "Epic";
-                }
-                else if (ChestLootRarityInt == 1001)
-                {
-                    ChestLootRarityString = "Legendary";
-                }
-            }
-            else if (ChestTypeInt > 799 && ChestTypeInt < 951)
-            {
-                ChestTypeString = "Silver";
 
-                if (ChestLootRarityInt < 201)
+                else if (ChestLootInt > WoodCommon + WoodUncommon && ChestLootInt <= WoodCommon + WoodUncommon + WoodRare)
                 {
-                    ChestLootRarityString = "Common";
+                    currentRarity = "Wood Rare";
                 }
-                else if (ChestLootRarityInt > 199 && ChestLootRarityInt < 551)
-                {
-                    ChestLootRarityString = "Uncommon";
-                }
-                else if (ChestLootRarityInt > 549 && ChestLootRarityInt < 851)
-                {
-                    ChestLootRarityString = "Rare";
-                }
-                else if (ChestLootRarityInt > 849 && ChestLootRarityInt < 1001)
-                {
-                    ChestLootRarityString = "Epic";
-                }
-                else if (ChestLootRarityInt == 1001)
-                {
-                    ChestLootRarityString = "Legendary";
-                }
-            }
-            else if (ChestTypeInt > 949 && ChestTypeInt < 1001)
-            {
-                ChestTypeString = "Gold";
 
-                if (ChestLootRarityInt < 101)
+                else if (ChestLootInt > WoodCommon + WoodUncommon + WoodRare && ChestLootInt <= WoodCommon + WoodUncommon + WoodRare + WoodEpic)
                 {
-                    ChestLootRarityString = "Common";
+                    currentRarity = "Wood Epic";
                 }
-                else if (ChestLootRarityInt > 99 && ChestLootRarityInt < 348)
-                {
-                    ChestLootRarityString = "Uncommon";
-                }
-                else if (ChestLootRarityInt > 346 && ChestLootRarityInt < 748)
-                {
-                    ChestLootRarityString = "Rare";
-                }
-                else if (ChestLootRarityInt > 746 && ChestLootRarityInt < 998)
-                {
-                    ChestLootRarityString = "Epic";
-                }
-                else if (ChestLootRarityInt > 997)
-                {
-                    ChestLootRarityString = "Legendary";
-                }
-            }
-            else if (ChestTypeInt == 1001)
-            {
-                ChestTypeString = "Platinum";
 
-                if (ChestLootRarityInt < 51)
+                else if (ChestLootInt > WoodCommon + WoodUncommon + WoodRare + WoodEpic && ChestLootInt <= WoodCommon + WoodUncommon + WoodRare + WoodEpic + WoodLegendary)
                 {
-                    ChestLootRarityString = "Common";
-                }
-                else if (ChestLootRarityInt > 49 && ChestLootRarityInt < 242)
-                {
-                    ChestLootRarityString = "Uncommon";
-                }
-                else if (ChestLootRarityInt > 240 && ChestLootRarityInt < 692)
-                {
-                    ChestLootRarityString = "Rare";
-                }
-                else if (ChestLootRarityInt > 690 && ChestLootRarityInt < 991)
-                {
-                    ChestLootRarityString = "Epic";
-                }
-                else if (ChestLootRarityInt > 990)
-                {
-                    ChestLootRarityString = "Legendary";
+                    currentRarity = "Wood Legendary";
                 }
             }
 
-            Debug.Log($"Chest Type Int: {ChestTypeInt}     Chest Loot Rarity Int: {ChestLootRarityInt}");
-            Debug.Log($"Chest Type = {ChestTypeString}     Chest Loot Rarity = {ChestLootRarityString}");
+            else if(ChestTypeInt > WoodChestChance && ChestTypeInt <= WoodChestChance + BronzeChestChance)
+            {
+                currentChest = BronzeChest;
 
-            #endregion
+                if (ChestLootInt <= BronzeCommon)
+                {
+                    currentRarity = "Bronze Common";
+                }
+
+                else if (ChestLootInt > BronzeCommon && ChestLootInt <= BronzeCommon + BronzeUncommon)
+                {
+                    currentRarity = "Bronze Uncommon";
+                }
+
+                else if (ChestLootInt > BronzeCommon + BronzeUncommon && ChestLootInt <= BronzeCommon + BronzeUncommon + BronzeRare)
+                {
+                    currentRarity = "Bronze Rare";
+                }
+
+                else if (ChestLootInt > BronzeCommon + BronzeUncommon + BronzeRare && ChestLootInt <= BronzeCommon + BronzeUncommon + BronzeRare + BronzeEpic)
+                {
+                    currentRarity = "Bronze Epic";
+                }
+
+                else if (ChestLootInt > BronzeCommon + BronzeUncommon + BronzeRare + BronzeEpic && ChestLootInt <= BronzeCommon + BronzeUncommon + BronzeRare + BronzeEpic + BronzeLegendary)
+                {
+                    currentRarity = "Bronze Legendary";
+                }
+            }
+
+            else if (ChestTypeInt > WoodChestChance + BronzeChestChance && ChestTypeInt <= WoodChestChance + BronzeChestChance + SilverChestChance)
+            {
+                currentChest = SilverChest;
+
+                if (ChestLootInt <= SilverCommon)
+                {
+                    currentRarity = "Silver Common";
+                }
+
+                else if (ChestLootInt > SilverCommon && ChestLootInt <= SilverCommon + SilverUncommon)
+                {
+                    currentRarity = "Silver Uncommon";
+                }
+
+                else if (ChestLootInt > SilverCommon + SilverUncommon && ChestLootInt <= SilverCommon + SilverUncommon + SilverRare)
+                {
+                    currentRarity = "Silver Rare";
+                }
+
+                else if (ChestLootInt > SilverCommon + SilverUncommon + SilverRare && ChestLootInt <= SilverCommon + SilverUncommon + SilverRare + SilverEpic)
+                {
+                    currentRarity = "Silver Epic";
+                }
+
+                else if (ChestLootInt > SilverCommon + SilverUncommon + SilverRare + SilverEpic && ChestLootInt <= SilverCommon + SilverUncommon + SilverRare + SilverEpic + SilverLegendary)
+                {
+                    currentRarity = "Silver Legendary";
+                }
+            }
+
+            else if (ChestTypeInt > WoodChestChance + BronzeChestChance + SilverChestChance && ChestTypeInt <= WoodChestChance + BronzeChestChance + SilverChestChance + GoldChestChance)
+            {
+                currentChest = GoldChest;
+
+                if (ChestLootInt <= GoldCommon)
+                {
+                    currentRarity = "Gold Common";
+                }
+
+                else if (ChestLootInt > GoldCommon && ChestLootInt <= GoldCommon + GoldUncommon)
+                {
+                    currentRarity = "Gold Uncommon";
+                }
+
+                else if (ChestLootInt > GoldCommon + GoldUncommon && ChestLootInt <= GoldCommon + GoldUncommon + GoldRare)
+                {
+                    currentRarity = "Gold Rare";
+                }
+
+                else if (ChestLootInt > GoldCommon + GoldUncommon + GoldRare && ChestLootInt <= GoldCommon + GoldUncommon + GoldRare + GoldEpic)
+                {
+                    currentRarity = "Gold Epic";
+                }
+
+                else if (ChestLootInt > GoldCommon + GoldUncommon + GoldRare + GoldEpic && ChestLootInt <= GoldCommon + GoldUncommon + GoldRare + GoldEpic + GoldLegendary)
+                {
+                    currentRarity = "Gold Legendary";
+                }
+            }
+
+            else if (ChestTypeInt > WoodChestChance + BronzeChestChance + SilverChestChance + GoldChestChance && ChestTypeInt <= WoodChestChance + BronzeChestChance + SilverChestChance + GoldChestChance + PlatinumChestChance)
+            {
+                currentChest = PlatinumChest;
+
+                if (ChestLootInt <= PlatinumCommon)
+                {
+                    currentRarity = "Platinum Common";
+                }
+
+                else if (ChestLootInt > PlatinumCommon && ChestLootInt <= PlatinumCommon + PlatinumUncommon)
+                {
+                    currentRarity = "Platinum Uncommon";
+                }
+
+                else if (ChestLootInt > PlatinumCommon + PlatinumUncommon && ChestLootInt <= PlatinumCommon + PlatinumUncommon + PlatinumRare)
+                {
+                    currentRarity = "Platinum Rare";
+                }
+
+                else if (ChestLootInt > PlatinumCommon + PlatinumUncommon + PlatinumRare && ChestLootInt <= PlatinumCommon + PlatinumUncommon + PlatinumRare + PlatinumEpic)
+                {
+                    currentRarity = "Platinum Epic";
+                }
+
+                else if (ChestLootInt > PlatinumCommon + PlatinumUncommon + PlatinumRare + PlatinumEpic && ChestLootInt <= PlatinumCommon + PlatinumUncommon + PlatinumRare + PlatinumEpic + PlatinumLegendary)
+                {
+                    currentRarity = "Platinum Legendary";
+                }
+            }
+
+            Debug.Log($"Current Chest = {currentChest}      Current Rarity = {currentRarity}");
         }
     }
 }
